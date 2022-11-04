7416bcc02671d7d69ccb40cbf81daad0190f0b34
Armixas <46973094+Armixas@users.noreply.github.com>
2022-11-04 21:02:18 +0200

    testas

--- BlazorApp1/BlazorApp1/Server/Controllers/WeatherForecastController.cs
+++ BlazorApp1/BlazorApp1/Server/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace BlazorApp1.Server.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Juodas", "Juodesnis"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
